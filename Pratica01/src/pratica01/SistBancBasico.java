f790c86ff78b1b5e3701f260ad92e06d5d3bb579
OtavioFinger <otaviofinger302@gmail.com>
2025-04-30 00:00:15 -0300

    Segundo true commit

--- Pratica01/src/pratica01/SistBancBasico.java
+++ Pratica01/src/pratica01/SistBancBasico.java
@@ -10,4 +10,62 @@ package pratica01;
  */
 public class SistBancBasico {
     
-}
+// Atributos
+    int numero;
+    String dono;
+    double saldo;
+    double limite;
+
+    // Métodos
+    void deposita(double quantidade) {
+        saldo = saldo + quantidade;
+    }
+    
+    boolean saca(double valor) {
+        if (saldo < valor) { //pro desafio é só trocar saldo + limite < valor
+            return false;
+        } else {
+            saldo = saldo - valor;
+            return true;
+        }
+    }
+
+    void imprimeDono(String dono){
+        System.out.println("Dono:" + dono);
+    }
+
+    void devolveSaldo(double saldo){
+        return saldo;
+    }
+
+    transferePara(SistBancBasico destino, double quantidade){
+        if(saca(quantidade)){
+            destino.deposita(quantidade);
+            return true;
+        }
+        return false;
+    }
+
+    void imprimeDados(){
+        System.out.print("Número: " + numero);
+        System.out.print("Dono: " + saldo);
+        System.out.print("Saldo: " + saldo);
+        System.out.print("Limite: " + limite + "\n");
+
+    }
+
+    public static void main(String[] args){
+        SistBancBasico maria, joao; //instanciando o objeto, é aqui que vamos testar se tudo funciona;
+       
+        maria = new SistBancBasico();
+        joao = new SistBancBasico();
+
+        maria.dono = "Maria";
+        maria.saldo = 1000;
+
+        maria.deposita(200);
+        System.out.println("Dono: " + maria.dono);
+        System.out.println("Saldo: " + maria.saldo); //sout tab faz automaticamente essa linha
+    }
+        
+    }
